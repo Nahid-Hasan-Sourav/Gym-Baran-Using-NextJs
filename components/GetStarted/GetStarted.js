e539600773b108a86e205d3753c0649bcfa20336
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2023-02-10 02:01:29 +0600

    solve responsive problem

--- components/GetStarted/GetStarted.js
+++ components/GetStarted/GetStarted.js
@@ -9,7 +9,7 @@ const GetStarted = () => {
             <div className=" flex items-center justify-center sm:px-0 px-7">
             <Image src={workOut} alt="image" width="80%" height="80%" ></Image>
             </div>
-            <div className="px-10 flex flex-col justify-center order-first ">
+            <div className="px-10 flex flex-col justify-center md:order-last order-first ">
                 <h2 className="font-logoFont font-extrabold text-5xl">Best full body<br></br> workout to lose fat</h2>
                 <p>Exercise is a very important need for our body. Health and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercising, either alone or with your friends at home. </p>
                 <div className="mt-6">
